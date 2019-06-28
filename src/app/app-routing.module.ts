3fea750fd2d84ad6a03afdb475cf41bfbd24da69
gouthamraja22 <gouthamraja22@gmail.com>
2019-06-28 16:09:19 +0530

    Initial Production

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {ExperienceComponent } from './experience/experience.component';
+import {EducationComponent} from './education/education.component';
+import {SkillsComponent} from './skills/skills.component';
+import {InterestsComponent} from './interests/interests.component';
+import {AwardsComponent } from './awards/awards.component';
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutComponent
 
-const routes: Routes = [];
+},
+{
+path:'experience',
+component:ExperienceComponent
+},
+{
+  path:'education',
+  component:EducationComponent
+},
+{
+path:'skills',
+component:SkillsComponent
+},
+{
+path:'interests',
+component:InterestsComponent
+},
+{
+path:"awards",
+component:AwardsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestsComponent } from './interests/interests.component';
+import { AwardsComponent } from './awards/awards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestsComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule,
